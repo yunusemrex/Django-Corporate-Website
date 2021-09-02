29cf73408846db4d5d59db4216e2e24ac13a7030
yunusemrekarakus <yunusemreiletisim23@gmail.com>
2021-09-02 22:22:41 +0300

    settings

---
+++ config/settings/development.py
@@ -0,0 +1,18 @@
+from config.settings.production import DATABASES
+from base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+AUTH_PASSWORD_VALIDATORS = [
+
+]
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
---
+++ config/settings/production.py
@@ -0,0 +1,55 @@
+from base import *
+
+
+ALLOWED_HOSTS = ['digitalkey.tech', '127.0.0.1', '46.31.79.37']
+
+DEBUG = False
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER':  env('DB_USER'),
+        'PASSWORD':  env('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'POST': '',
+    }
+}
+
+
+
+
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 86400
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
